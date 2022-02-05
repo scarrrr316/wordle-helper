14944b0fc4d7d266213c761ec5958d806c380e04
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2022-02-06 07:06:07 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,13 @@
-// Shuffle the suggestions given
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-// Run when the popup is clicked 
+// Run when the popup is clicked
 document.addEventListener('DOMContentLoaded', async () => {
-  const numWords = document.getElementById('numWords');
-  const possibleHTML = document.getElementById('possible');
+  const title = document.getElementById('title');
+  const answerHTML = document.getElementById('answer');
 
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  await chrome.tabs.sendMessage(tab.id, {}, ({ possible }) => {
-    shuffleArray(possible);
-    numWords.innerHTML = `${possible.length} possible word${possible.length > 1 ? 's' : ''}`;
-    const suggestions = possible.map(word => `${word.toUpperCase()}`).join(', ');
-    possibleHTML.innerHTML = suggestions;
-    chrome.action.setBadgeText({ text: `${possible.length || 1}`, tabId: tab.id });
+  await chrome.tabs.sendMessage(tab.id, {}, ({ answer }) => {
+    title.innerHTML = `Answer`;
+    answerHTML.innerHTML = answer.map(word => `${word.toUpperCase()}`).join(', ');
+    chrome.action.setBadgeText({ text: `${answer.length || 1}`, tabId: tab.id });
     chrome.action.setBadgeBackgroundColor({ color: '#538d4e' });
   });
 })

bb641319e1ad6e809284930d64c9f0ea5a0d044f
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2022-02-06 21:48:06 +0800

    Update solver.js

--- util/solver.js
+++ util/solver.js
@@ -6,12 +6,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     try {
         let answer = solve();
         let suggestion = answer[0];
-        // sendResponse({ suggestion, answer });
         sendResponse({answer});
     } catch (e) {
         console.error("encountered JSON parse error", e);
     }
 });
 
-// Run when wordle page first opens
 chrome.runtime.sendMessage(solve());

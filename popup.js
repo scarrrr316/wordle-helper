72d9271af2eef9cab4fa7398143384c922f9e5c0
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2022-02-06 21:40:10 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,8 +6,16 @@ document.addEventListener('DOMContentLoaded', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   await chrome.tabs.sendMessage(tab.id, {}, ({ answer }) => {
     title.innerHTML = `Answer`;
-    answerHTML.innerHTML = answer.map(word => `${word.toUpperCase()}`).join(', ');
+    answerHTML.innerHTML = `<p>${answer.map(word => `${word.toUpperCase()}`).join(', ')}</p>`;
     chrome.action.setBadgeText({ text: `${answer.length || 1}`, tabId: tab.id });
     chrome.action.setBadgeBackgroundColor({ color: '#538d4e' });
   });
 })
+
+function injectTheScript() {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['sim_input.js']})
+  })
+}
+
+document.getElementById('answer').addEventListener('click', injectTheScript);
